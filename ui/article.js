5a8765ef95d9d1273c255da678457f8acd15b67a
kayalvizhi <vizhiyalm@gmail.com>
2017-03-17 12:03:59 +0530

    Create article.js

---
+++ ui/article.js
@@ -0,0 +1,64 @@
+// Eg: url/articles/article-one will result in article-one
+var currentArticleTitle = window.location.pathname.split('/')[2];
+
+
+
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadCommentForm(this.responseText);
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+function escapeHTML (text)
+{
+    var $text = document.createTextNode(text);
+    var $div = document.createElement('div');
+    $div.appendChild($text);
+    return $div.innerHTML;
+}
+
+function loadComments () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var comments = document.getElementById('comments');
+            if (request.status === 200) {
+                var content = '';
+                var commentsData = JSON.parse(this.responseText);
+                for (var i=0; i< commentsData.length; i++) {
+                    var time = new Date(commentsData[i].timestamp);
+                    content += `<div class="comment">
+                        
+                        <div class="commenter">
+                            <strong>${commentsData[i].username}</strong> made a comment<br/> 
+                            <h5>${time.toLocaleTimeString()} on ${time.toLocaleDateString()} </h5>
+                        </div>
+                        <p>${escapeHTML(commentsData[i].comment)}</p>
+                        <hr>
+                    </div>`;
+                }
+                comments.innerHTML = content;
+            } else {
+                comments.innerHTML('Oops! Could not load comments!');
+            }
+        }
+    };
+    
+    request.open('GET', '/get-comments/' + currentArticleTitle, true);
+    request.send(null);
+}
+
+
+// The first thing to do is to check if the user is logged in!
+loadLogin();
+loadComments();
